866f57569af037eff78221cdf4b069532b29b939
Wiktor Zykubek <wz-git@mailbox.org>
2023-07-10 18:05:59 +0200

    Fix building on Windows

--- prebuild.py
+++ prebuild.py
@@ -3,8 +3,11 @@
 import sys
 import os
 from shutil import which
+import logging
 
 UI_DIR = "assets/ui"
+
+logging.basicConfig(level=logging.INFO)
 
 
 class UICNotFound(Exception):
@@ -17,13 +20,21 @@ if sys.platform == "linux":
 elif sys.platform == "win32":
     exe = "pyside6-uic.exe"
     exe_alt = None
+    UI_DIR = UI_DIR.replace("/", "\\")
 
 if not which(exe) and not which(exe_alt):
     raise UICNotFound("User Interface Compiler binary not found.")
 elif not which(exe) and which(exe_alt):
     exe = exe_alt
 
+logging.info("Building UI files...")
 for f in os.listdir(UI_DIR):
     if exe == "/usr/lib/qt6/uic":
         __flags = "--generator python"
-    os.system(f"{exe} {UI_DIR}/{f} -o openfm_qt/ui_{f[:-3]}.py {__flags}")
+    else:
+        __flags = ""
+    cmd = f"{exe} {UI_DIR}/{f} -o openfm_qt/ui_{f[:-3]}.py {__flags}"
+    if sys.platform == "win32":
+        cmd = cmd.replace("/", "\\")
+    os.system(cmd)
+    logging.info(f"Successfully builded {f} as ui_{f[:-3]}.py")
