d72d8c751d94fd34b1bcd67bf60787fd627476cb
Wiktor Zykubek <wz-git@mailbox.org>
2023-07-11 15:16:40 +0200

    Update .gitignore and format __main__.py

--- openfm_qt/__main__.py
+++ openfm_qt/__main__.py
@@ -1,14 +1,16 @@
-# This Python file uses the following encoding: utf-8
+# coding=utf-8
 
 from PySide6.QtWidgets import QApplication
 from . import MainWindow
 import sys
+
 
 def main():
     app = QApplication(sys.argv)
     widget = MainWindow()
     widget.show()
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
